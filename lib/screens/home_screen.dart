451c7731760958a52c1a3870d618f68dc88af740
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-07-19 22:14:46 -0400

    HomeScreen Search Bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/data/data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +11,59 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Food Delivery'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.circle),
+          iconSize: 30.0,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text('Cart(${currentUser.cart!.length})'),
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                      width: 0.8, color: Theme.of(context).primaryColor),
+                ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30.0,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                    size: 30.0,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
