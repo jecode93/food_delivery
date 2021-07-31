a82d26264a64e40cd269da443516ef29f6239dd3
jec-ode93 <jeanemmanuelcadet@gmail.com>
2021-07-30 22:32:45 -0400

    Working on the BottomSheet Checkout Button

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -107,7 +107,7 @@ class _CartScreenState extends State<CartScreen> {
             Container(
               margin: EdgeInsets.all(10.0),
               child: Text(
-                '\$${orderItem.quantity! * orderItem.food!.price!.toInt()}',
+                '\$${orderItem.quantity! * orderItem.food!.price!}',
                 style: TextStyle(
                   fontSize: 16.0,
                   fontWeight: FontWeight.w600,
@@ -122,22 +122,96 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart!.forEach(
+      (Order order) => totalPrice += (order.quantity! * order.food!.price!),
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart (${currentUser.cart!.length})'),
       ),
       body: ListView.separated(
         itemBuilder: (context, index) {
-          Order order = currentUser.cart![index];
-          return _buildCartItem(order);
+          if (index < currentUser.cart!.length) {
+            Order order = currentUser.cart![index];
+            return _buildCartItem(order);
+          }
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Estimated Delivery Time:',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      '25 min',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total Cost:',
+                      style: TextStyle(
+                          fontSize: 20.0, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      '\$${totalPrice.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.green[700],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 80.0),
+              ],
+            ),
+          );
         },
-        itemCount: currentUser.cart!.length,
+        itemCount: currentUser.cart!.length + 1,
         separatorBuilder: (context, index) {
           return Divider(
             height: 1.0,
             color: Colors.grey,
           );
         },
+      ),
+      bottomSheet: Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width,
+        decoration:
+            BoxDecoration(color: Theme.of(context).primaryColor, boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, -1),
+            blurRadius: 6.0,
+          )
+        ]),
+        child: Center(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'CHECKOUT',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 22.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
